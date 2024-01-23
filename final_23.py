051da47508a3c35849e37f43af76f522a986eec5
YonkaMayonkaZ <danielmavroudis@gmail.com>
2024-01-23 19:34:42 +0200

    first commit

---
+++ final_23.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+import warnings
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+# Import the datasets
+tracks = pd.read_csv("tracks.csv")
+ = pd.read_csv("tracks.csv")
